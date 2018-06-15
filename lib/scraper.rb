7fdfd35607131585622bf652c2c498edb72dbb0d
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-15 15:35:12 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,6 +15,8 @@ class Scraper
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
 
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
     end
   end
 end 
